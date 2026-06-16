a508507ac2c576947351b3c8f456a4132853f9e7
Gohans-amazing-tutor <gohgon555@gmail.com>
2026-06-16 18:52:04 -0500

    Add games database with multiple game entries

---
+++ Game.js
@@ -0,0 +1,6 @@
+const gamesDatabase = [
+    { name: "Space Shooter", category: "Action", image: "space.jpg", link: "games/space/index.html" },
+    { name: "Block Puzzle", category: "Puzzle", image: "puzzle.jpg", link: "games/puzzle/index.html" },
+    { name: "Sniper Alley", category: "FPS", image: "sniper.jpg", link: "games/fps/index.html" }
+];
+
